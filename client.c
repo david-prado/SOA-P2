222476da3f1487dd1afe773fc16b277c8dea55c6
David Prado <dprado84@gmail.com>
2015-04-18 19:59:51 -0600

    Secuential version almost ready

--- client.c
+++ client.c
@@ -1,8 +1,52 @@
-/*
- * client.c
- *
- *  Created on: Apr 8, 2015
- *      Author: davidp
- */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include "lib/error/error.h"
 
+int main(int argc, char *argv[])
+{
+    int sockfd, portno, n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
 
+    char buffer[256];
+    if (argc < 3) {
+       fprintf(stderr,"Usage: %s hostname port\n", argv[0]);
+       exit(0);
+    }
+    portno = atoi(argv[2]);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+        exitError("ERROR: could not open socket",1);
+    server = gethostbyname(argv[1]);
+    if (server == NULL)
+        exitError("ERROR: no such host",1);
+
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr,
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+    serv_addr.sin_port = htons(portno);
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
+        exitError("ERROR: could not connect to server",1);
+
+    printf("Please enter the message: ");
+    bzero(buffer,256);
+    fgets(buffer,255,stdin);
+    n = write(sockfd,buffer,strlen(buffer));
+    if (n < 0)
+         exitError("ERROR: could not write to socket",1);
+    bzero(buffer,256);
+    n = read(sockfd,buffer,255);
+    if (n < 0)
+         exitError("ERROR: could not read from socket",1);
+    printf("%s\n",buffer);
+    close(sockfd);
+    return 0;
+}
--- secuential.c
+++ secuential.c
@@ -1,8 +1,23 @@
-/*
- * secuential.c
- *
- *  Created on: Apr 8, 2015
- *      Author: davidp
- */
+#include <stdlib.h>
+#include "lib/error.h"
+#include "lib/http.h"
+#include "lib/socket.h"
+
+void requestHandler(int cliSock){
+	handleHttpRequest(cliSock);
+	//closeWriteSock(cliSock);
+}
+
+int main(int argc, char *argv[]) {
+	int srvPort;
+
+	if (argc < 2)
+		exitError("ERROR: port number not provided\n",1);
+
+	srvPort = atoi(argv[1]);
+	startServer(srvPort,&requestHandler);
+
+	return 0;
+}
 
 
